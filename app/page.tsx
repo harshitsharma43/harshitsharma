716fdd3414330e1844ca8115a0e35caaad920af0
Harshit Sharma <harshitsharma937@gmail.com>
2024-07-01 00:26:13 +0530

    changes mobile number updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
    <main className="max-w-3xl mx-auto py-10 my-10">
     <h1 className="text-3xl underline underline-offset-4 text-gray-800">Welcome to Harshit Sharma Personal Space</h1>
     <p className="text-sm text-gray-700">Visit here in future to get more update</p>
-    <p className="text-sm text-gray-700">Contact me here - 7275738085</p>
+    <p className="text-sm text-gray-700">Contact me here - +917275738085</p>
    </main>
   );
 }
